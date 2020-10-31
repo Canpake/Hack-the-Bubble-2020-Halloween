66038e2da230aafb34be9f01c1d5d1a7b5553317
Canpake <seeminglyobvioushuh@gmail.com>
2020-10-31 15:35:12 +0000

    label for player number

--- src/main.py
+++ src/main.py
@@ -6,15 +6,19 @@ class Menu(object):
 
     def __init__(self):
         self.root = Tk()
+        # self.root.geometry("300x300")
 
         self.draw_button = Button(self.root, text='Start Game', command=self.open_paint)
-        self.draw_button.grid(row=0, column=0)
+        self.draw_button.grid(row=0, column=0, columnspan=2)
+
+        self.player_text = Label(self.root, text='Number of players:')
+        self.player_text.grid(row=1, column=0)
 
         self.player_count = StringVar(self.root)
         self.player_count.set(3)
 
         self.player_select = OptionMenu(self.root, self.player_count, 3, 4, 5, 6, 7, 8)
-        self.player_select.grid(row=0, column=1)
+        self.player_select.grid(row=1, column=1)
 
         self.root.mainloop()
 
