bf61cf7048cd43cda12bd469a02653aa7942a132
ht67810 <hthompson67810@gmail.com>
2020-10-31 12:15:57 +0000

    Added get_term, gets random combination

--- examples/wordlist.py
+++ examples/wordlist.py
@@ -1,6 +1,16 @@
+import random
+
 adjectives = ["frightening", "scary", "spooky", "shivering", "eerie", "sinister", "hairy",
               "stumbling", "crying", "ghastly", "bloody", "vile", "howling", "ancient",
               "ominous", "dark", "filthy", "abandoned", "looming", "silent", "decaying"]
 nouns = ["pumpkin", "monster", "skeleton", "witch", "brew", "zombie", "ghoul",
          "ghost", "hag", "beast", "werewolf", "eye", "cobweb", "spider", "snake"]
 descriptor = ["blood", "slime", "terror", "fright", "evil", "fear", "death"]
+
+
+def get_term():
+    return (
+        random.choice(adjectives) + " " +
+        random.choice(nouns) + " of " +
+        random.choice(descriptor)
+    )
