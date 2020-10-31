17f71f331fcc787418f57452a5e1b079f51200e7
ht67810 <hthompson67810@gmail.com>
2020-10-31 11:40:28 +0000

    Word list

---
+++ examples/wordlist.py
@@ -0,0 +1,6 @@
+adjectives = ["frightening", "scary", "spooky", "shivering", "eerie", "sinister", "hairy",
+              "stumbling", "crying", "ghastly", "bloody", "vile", "howling", "ancient",
+              "ominous", "dark", "filthy", "abandoned", "looming", "silent", "decaying"]
+nouns = ["pumpkin", "monster", "skeleton", "witch", "brew", "zombie", "ghoul",
+         "ghost", "hag", "beast", "werewolf", "eye", "cobweb", "spider", "snake"]
+descriptor = ["blood", "slime", "terror", "fright", "evil", "fear", "death"]
