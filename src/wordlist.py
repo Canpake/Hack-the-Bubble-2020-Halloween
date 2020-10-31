a9cda52644ac23a0ea255d3e5425ddc3d565a2d2
death-cabbage <nicolecizauskas15@gmail.com>
2020-10-31 15:12:46 +0000

    extra words for wordlist 3

--- src/wordlist.py
+++ src/wordlist.py
@@ -7,7 +7,9 @@ adjectives = ["frightening", "scary", "spooky", "shivering", "eerie", "sinister"
               "hairless", "spoopy", "dripping", "crawling", "thirsty", "munching", "hungry", "wicked", "haunting",
               "banished", "crazed", "rabid", "threatening", "growing", "flailing", "crunchy", "gross", "invisible",
               "screaming", "shouting", "laughing", "menacing", "twisted", "banned", "illegal", "lifeless", "fatigued",
-              "troubled", "agonized", "charred", "freakish", "brutish", "colourless", "pale"]
+              "troubled", "agonized", "charred", "freakish", "brutish", "colourless", "pale", "melting", "spineless",
+              "torn", "spiked", "wild", "stormy", "screeching", "trembling", "dusty", "awoken", "bubbling", "slithering",
+              "creeping", "exploding", "whispering", "cursed", "destroyed", "demolished", "poisonous", "venomous"]
 nouns = ["pumpkin", "monster", "skeleton", "witch", "brew", "zombie", "ghoul",
          "ghost", "hag", "beast", "werewolf", "eye", "cobweb", "spider", "snake", "spider", "flesh", "vampire", "smoke",
          "fire", "demon", "cyborg", "robot", "worm", "centipede", "nest", "grave", "tomb", "coffin", "meat", "feet",
@@ -15,15 +17,19 @@ nouns = ["pumpkin", "monster", "skeleton", "witch", "brew", "zombie", "ghoul",
          "spine", "limb", "bones", "trees", "fingers", "villain", "caramel apple", "candy apple", "bug", "weapon",
          "crevice", "being", "humanoid", "gamer", "horse", "centaur", "hooves", "cryptid", "corn stalk", "corn field",
          "feast", "bigfoot", "chupacabra", "tentacle", "homicide", "mystery", "bite", "injury", "hole", "eclipse",
-         "moth", "pitchfork", "cannibal", "alien", "U.F.O", "creation", "concoction", "wife"]
+         "moth", "pitchfork", "cannibal", "alien", "U.F.O", "creation", "concoction", "wife", "Eldritch horror", "human",
+         "stampede", "flock", "briefcase", "elbow", "bone marrow", "crow", "circus"]
 descriptor = ["blood", "slime", "terror", "fright", "evil", "fear", "death", "coursework",
               "failing my degree", "depression", "sadness", "rot", "bodily fluids", "bile", "drool", "mourning", "hell",
               "doom", "misery", "tears", "deadlines", "assignments", "wetness", "the presidential election", "St. Andrews",
               "Andrew Melville Hall", "mud", "darkness", "madness", "anger", "unethical business", "not paying taxes",
               "Hack-the-Bubble","STACS", "moistness", "tax evasion", "burglary", "identity theft", "loss", "gaming",
-              "Eldritch horror", "piracy", "Fife", "2020", "midnight", "the full moon", "a minor car accident",
+               "piracy", "Fife", "2020", "the full moon", "a minor car accident", "existence",
               "broken mirrors", "space", "the void", "nothingness", "apathy", "unaffordable accommodation", "nihilism",
-              "broken hearts", "loneliness", "not having a wife"]
+              "broken hearts", "loneliness", "not having a wife", "putting pickles in a blender", "dampness",
+              "drinking pickles from a blender", "wearing unmatched socks", "the night", "computer science",
+              "parallel parking", "cold big macs", "not having a girlfriend", "minecraft", "goo", "nightmares", "chaos",
+              "disorder", "unsustainable farming", "disgrace"]
 
 
 def get_term():
@@ -32,3 +38,8 @@ def get_term():
         random.choice(nouns) + " of " +
         random.choice(descriptor)
     )
+
+if __name__ == '__main__':
+    print(get_term())
+    print(get_term())
+    print(get_term())
